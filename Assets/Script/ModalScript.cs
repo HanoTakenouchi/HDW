f47690516e517c1e1796a41ff6f6714d7dcb1e57
Hano TAKENOUCHI <>
2018-06-19 18:36:40 +0900

    モーダルをz軸-60で出る、消えるできた

--- Assets/Script/ModalScript.cs
+++ Assets/Script/ModalScript.cs
@@ -6,16 +6,25 @@ using UnityEngine.Animations;
 
 public class ModalScript : MonoBehaviour
 {
-
-	Animation SousaAnimation;
-	GameObject SousaGroup;
-
+    
+	Animation SousaGroupAnimation;
+	Animation Sousa2GroupAnimation;
+	public GameObject SousaGroup;
+	public GameObject Sousa2Group;
+	public GameObject Robo;
+	private bool flag;
+	private bool flag2;
+	private bool flag3;
+	private bool fig;
 	// Use this for initialization
 	void Start()
 	{
-		SousaGroup = GameObject.Find("/CanvasGroup/SousaCanvas/SousaGroup");
-		SousaAnimation = SousaGroup.gameObject.GetComponent<Animation>();
-		SousaAnimation.Play();
+		SousaGroupAnimation = SousaGroup.gameObject.GetComponent<Animation>();
+		Sousa2GroupAnimation = Sousa2Group.gameObject.GetComponent<Animation>();
+		SousaGroupAnimation.Play();
+		Sousa2GroupAnimation.gameObject.SetActive(false);
+		flag = true;
+		flag2 = true;
 	}
 
 	// Update is called once per frame
@@ -23,7 +32,36 @@ public class ModalScript : MonoBehaviour
 	{
 		if (Input.GetKey(KeyCode.L))
 		{
-			SousaAnimation.gameObject.SetActive(false);
+			SousaGroupAnimation.gameObject.SetActive(false);
+		}
+
+		if (Robo.gameObject.transform.position.z >= -60)
+		{
+			if (flag)
+			{
+				Debug.Log("Sousa1");
+				Sousa2GroupAnimation.gameObject.SetActive(true);
+				flag = false;
+				flag2 = true;
+				Debug.Log("Sousa2");
+			}
+			if (flag2)
+            {
+                Sousa2GroupAnimation.Play();
+                Debug.Log("Sousa3");
+                flag2 = false;
+                flag3 = true;
+            }
+            if (flag3)
+            {
+                if (Input.GetKey(KeyCode.L))
+                {
+                    Debug.Log("Sousa4");
+                    Sousa2GroupAnimation.gameObject.SetActive(false);
+                    Debug.Log("Sousa5");
+                    flag3 = false;
+                }
+            }
 		}
 	}
 }
