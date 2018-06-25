d9d406255bcece71e7362eb0494cdd3bbee346e7
Hano TAKENOUCHI <>
2018-06-25 22:31:57 +0900

    ステージ選択シーンから今出来てるシーンに飛べるようにした

---
+++ Assets/Script/SceneManagerScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript: MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void KaigiClick()
+	{
+		SceneManager.LoadScene("Main");
+	}
+
+	public void KenkyuuClick()
+	{
+		SceneManager.LoadScene("");
+	}
+
+	public void DataClick()
+	{
+		SceneManager.LoadScene("");
+	}
+
+	public void AIClick()
+	{
+		SceneManager.LoadScene("");
+	}
+
+	public void ModoruClick()
+	{
+		SceneManager.LoadScene("Title");
+	}
+
+}
